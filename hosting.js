b380d82120a29fdba4c48c92ec53560ee7ca5dbd
Herman van Rink <rink@initfour.nl>
2014-08-22 16:30:45 +0200

    Issue #2223033 by helmo: Overlay close by ESC.

--- hosting.js
+++ hosting.js
@@ -11,5 +11,11 @@ Drupal.behaviors.hostingTaskLogAttach = {
   }
 }
 
+// Close the an open overlay window when hitting the ESC key
+// Example from https://www.drupal.org/node/963988
+// TODO make it configurable
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) { $('#overlay-close').click(); }
+});
 
 })(jQuery);
